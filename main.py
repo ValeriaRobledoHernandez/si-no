691458ffba75ea1e8a970df10496f4bfc40cd408
ValeriaRobledoHernandez <robledohernandezvaleria@cetis50cdmx.com>
2024-09-09 18:05:02 -0600

    se sube la version final

--- main.py
+++ main.py
@@ -3,33 +3,48 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "¿Me perdonas?"
-    page.bgcolor="Green"
+    page.bgcolor="red"
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
     page.vertical_alignment=ft.MainAxisAlignment.CENTER
     lbl1=ft.Text('¿Me perdonas?',
                 style=ft.TextStyle(size=40,weight='bold'))
     img1=ft.Image(src='triste.png',width=200,height=200)
-    btnSi=ft.ElevateButton('Si',
-                            color='green',
+    btnSi=ft.ElevatedButton('Si',
+                            color='blue',
                             width=100,
                             height=50
                             )
     btnNo=ft.ElevatedButton('No',
-                            color="red",
+                            color="purple",
                             width=100,
-                            height=50)
+                            height=50
+                            )
+    def no(e):
+        btnSi.width+=30
+        btnNo.height+=10
+        page.update()
+    def si(e):
+        img1.src="Feliz.png"
+        page.update()
+    btnNo.on_click=no
+    btnSi.on_click=si
     page.add(
         ft.Column(
             [
                 lbl1,
                 img1,
                 ft.Row(
-                    [btnSi,btnNo]
-                )
-            ]
+                    [btnSi,btnNo],
+                    alignment=ft.MainAxisAlignment.CENTER
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=20
         )
     )
 
 
 
 ft.app(main)
+
